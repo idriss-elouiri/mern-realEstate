7f89c67b950dd9f4375c9f0714a625ebb6295d3e
conta idriss <idriss1elouiri2@gmail.com>
2024-09-10 13:05:02 +0100

    save changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://api-real-estate-gules.vercel.app",
         secure: false,
       },
     },
